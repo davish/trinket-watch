ccff5a21b79e7d097b78625626728e26bc9374ab
Davis Haupt <dbh937@gmail.com>
2014-12-18 23:22:21 -0500

    initial commit

---
+++ TrinketWatch.ino
@@ -0,0 +1,98 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x64 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+  
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+ Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.display();
+  delay(200);
+  display.clearDisplay();
+  // text display tests
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+}
+int hours = 9;
+int minutes = 50;
+
+void loop() {
+  demo();
+}
+
+void demo() {
+  display.clearDisplay();
+  if (minutes >= 60) {
+    hours++;
+    minutes = 0;
+  }
+  if (hours > 23)
+    hours = 1;
+  if (hours < 10)
+    display.setCursor(17,10);
+  else
+    display.setCursor(5,10);
+  display.print(hours, DEC);
+  display.print(":");
+  if (minutes < 10)
+    display.print("0");
+  display.print(minutes, DEC);
+  display.println();
+  display.display();
+  delay(200);
+  
+  minutes++;
+}
