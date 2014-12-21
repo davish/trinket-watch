e45845bae0c8907dce8c64fe181f59ed0c4d544a
Davis Haupt <dbh937@gmail.com>
2014-12-21 16:18:47 -0400

    added Adafruit License

--- emulator/emulator.js
+++ emulator/emulator.js
@@ -21,14 +21,14 @@ function step() {
 
 function smile() {
   // face
-  gfx.drawCircle(64, 32, 10);
+  display.drawCircle(64, 32, 10);
 
   // eyes
   drawPixel(60, 28);
   drawPixel(68, 28);
 
   // mouth
-  gfx.drawLine(60, 35, 68, 35);
+  display.drawLine(60, 35, 68, 35);
   drawPixel(60, 34);
   drawPixel(68, 34);
 }
@@ -70,9 +70,23 @@ function millis() {
 /*
   Functions ported from the Adafruit Graphics Library
   https://github.com/adafruit/Adafruit-GFX-Library
+
+
+  Software License Agreement (BSD License)
+
+  Copyright (c) 2012 Adafruit Industries.  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  - Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
 */
 
-var gfx = {
+var display = {
   drawCircle: function(x0, y0, r, color) {
     if (color)
       color = true;
